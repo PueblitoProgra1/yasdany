f09f6cddcb00be2b970f8eca2d0ef4c73d245e56
jeferyasda <yasdanymasa@gmail.com>
2025-02-01 15:40:03 -0600

    Add files via upload

---
+++ Declaraciones_Operadores/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double a = 5;
+        double b;
+        b = 7;
+        double c = b / a;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    }
